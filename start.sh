1ab819ec9adcd69cf62ac974b51eb6d1a9452db7
jothflee <62022524+jothflee@users.noreply.github.com>
2025-01-13 12:18:45 -0600

    chore: update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,10 @@ certfile=${CERTFILE:-/certs/server.pem}
 keyfile=${KEYFILE:-/certs/server.key}
 port=${PORT:-8008}
 
+gunicorn_cmd="gunicorn app:app --bind 0.0.0.0:$port --env BASIC_AUTH_USERNAME=$BASIC_AUTH_USERNAME --env BASIC_AUTH_PASSWORD=$BASIC_AUTH_PASSWORD --env FLASK_ENV=production --env OPENAI_API_KEY=$OPENAI_API_KEY --workers 8 --preload"
+
 if [ -f "$certfile" ] && [ -f "$keyfile" ]; then
-    gunicorn app:app --bind 0.0.0.0:$port --env FLASK_ENV=production --keyfile $keyfile --certfile $certfile --workers 8 --preload
-else
-    gunicorn app:app --bind 0.0.0.0:$port --env FLASK_ENV=production --workers 8 --preload
+    gunicorn_cmd="$gunicorn_cmd --keyfile $keyfile --certfile $certfile"
 fi
+
+exec $gunicorn_cmd
