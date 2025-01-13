3c8cfeb6a217141d685ee4cff11080c74de011ba
jothflee <62022524+jothflee@users.noreply.github.com>
2025-01-13 08:24:42 -0600

    chore: modify start.sh to fix port bindings

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ keyfile=${KEYFILE:-/certs/server.key}
 port=${PORT:-8443}
 
 if [ -f "$certfile" ] && [ -f "$keyfile" ]; then
-    gunicorn app:app --bind 0.0.0.0:8080 --bind 0.0.0.0:$port --env FLASK_ENV=production --keyfile $keyfile --certfile $certfile --workers 8 --preload
+    gunicorn app:app --bind 0.0.0.0:$port --env FLASK_ENV=production --keyfile $keyfile --certfile $certfile --workers 8 --preload
 else
-    gunicorn app:app --bind 0.0.0.0:8080 --env FLASK_ENV=production --workers 8 --preload
+    gunicorn app:app --bind 0.0.0.0:$port --env FLASK_ENV=production --workers 8 --preload
 fi
